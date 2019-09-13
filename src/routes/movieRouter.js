ff418d84142cd1e733cf0df03cdca4d0ca5ac559
Drew Palazzari <drewpalazzari@hotmail.com>
2019-09-13 14:55:30 -0600

    Refactor resObj definition

--- src/routes/movieRouter.js
+++ src/routes/movieRouter.js
@@ -4,13 +4,13 @@ const { Movie } = require('../db/movie');
 const { Rating } = require('../db/rating');
 const { formatResponse, formatBudgetValues } = require('../helpers/helpers');
 const { moviesValidator, movieValidator } = require('../middleware/movieRoutesMiddleware');
+const resObj = formatResponse();
 
 // ceb9bf8c
 
 module.exports = (app) => {
 
   app.get('/api/v1/movies', moviesValidator, async (req, res) => {
-    const resObj = formatResponse();
     const page = req.query.page;
     const year = req.query.year;
     let movie = new Movie(page, year);
@@ -21,13 +21,11 @@ module.exports = (app) => {
       res.status(200).send(resObj);
     } catch(err){
       util.log(`ERROR: ${err}`);
-    }
-    
+    } 
   })
 
   app.get('/api/v1/movies/:id', movieValidator, async (req, res) => {
     const movieId = parseInt(req.params.id);
-    const resObj = formatResponse();
     try {
       const movie = new Movie();
       const movieData = await movie.getMovie(movieId);
